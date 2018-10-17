f5b1135a513d30c76b14600d727a638d7c5696c3
md1024 <mdev1024@mail.com>
2018-10-17 00:51:43 -0400

    updated arduino code for targets, circuit diagram

--- arduino/target/target.ino
+++ arduino/target/target.ino
@@ -1,13 +1,15 @@
 #include <FastLED.h>
 
 
-#define NUM_LEDS 60
+#define NUM_LEDS 28
 #define CLOCK_PIN 1
 #define DATA_PIN 0
 #define PIEZO_PIN A1
 
 int threshold = 500;
 int readcycle = 10;
+
+int impact_max = 1024/8;
 
 CRGB leds[NUM_LEDS];
 void setup()
@@ -20,6 +22,7 @@ void setup()
       FastLED.show();
       leds[dot] = CRGB::Black;
     }
+    delay(1000);
 }
 
 int getPizeoReading() {
@@ -29,13 +32,10 @@ int getPizeoReading() {
   }
   return sum / readcycle;
 }
-void loop()
-{
-  
+void loop() {
         int piezo_read = getPizeoReading();
-
-        int tgtled = map(piezo_read, 0, 1024, 0, NUM_LEDS);
-        
+        piezo_read = min(piezo_read, impact_max);
+        int tgtled = map(piezo_read, 0, impact_max, 0, NUM_LEDS);
 
           for(int dot = 0; dot < tgtled; dot++) { 
           leds[dot] = CRGB::Blue;
@@ -44,7 +44,11 @@ void loop()
           for(int dot=tgtled;dot<NUM_LEDS;dot++) {
             leds[dot] = CRGB::Red;
           }
+
           leds[tgtled] = CRGB::Yellow;
-                FastLED.show();
+          FastLED.show();
+          if(piezo_read > (impact_max * 0.75)) {
+            delay(4000);
+          }
 
 }
