480354e4d0518d8e2a9877d960df09a5fecec65c
Drew Kario <kario@apostrophed.com>
2019-09-23 20:57:12 -0400

    Added a base game state to build from

---
+++ game_states/BaseGameState.py
@@ -0,0 +1,40 @@
+import pygame, sys
+from common.Drawable import Image, TextField
+from common.Common import globalVars as gv
+from common.Common import *
+from common.Inputs import gameController as gc
+import time
+import random
+
+class Timeout:
+  def __init__(self):
+    pass
+    #self.background = Image("background_timeout", "quickdraw_timeout.jpg", 0,0, True )
+    #
+    # self.gameObjects = []
+    # clintImg = Image("fore_clint", "clint.png", 0,250, True )
+    # clintObj = GameObject(10,10,True,clintImg)
+    # self.gameObjects.append(clintObj)
+    #
+    # self.text = TextField("Example_text", "Initial text, never seen", 0, 0, True, color=(255,255,255))
+    # 
+
+  def enter(self):
+    # self.text.text = "Text for the player to see"
+    pass
+    
+
+  def processEvents(self):
+    pass
+
+  def update(self, deltaTime):
+    # for obj in self.gameObjects:
+    # 	obj.update(deltaTime)
+    pass
+
+  def draw(self):
+    #self.background.draw(gv.screen)
+    pass
+    
+  def leave(self):
+    pass
